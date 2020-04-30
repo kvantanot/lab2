22e07c99c2d9fff4273b1f8ee5d33209f07730ac
Sergey <kvapuluk71@gmail.com>
2020-04-30 23:07:52 +0300

    First variables

---
+++ lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Hello World";//Ссылочный
+            int i = 1; //Значимый 
+            bool b; //Значимый
+            b = true;
+            int i1 = i;//Значимый
+            double d = 4.20 + i1;//Значимый
+            float f = default(float);//Значимый
+        }
+    }
+}
